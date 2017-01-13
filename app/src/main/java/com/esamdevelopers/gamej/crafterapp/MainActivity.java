6561de96bb40b7772501cf4b53b0c72e04836a30
Alberto Mejia <g.a.mejia@outlook.com>
2017-01-12 23:52:18 -0600

    Creacion de layouts (app_bar_main, app_bar_main_with_tabs, content_main, nav_header_main) ademas de la implementacion del DrawerLayout y el NavigationView en el xml activity_main, ademas de la creacion de la clase MainFragment

--- app/src/main/java/com/esamdevelopers/gamej/crafterapp/MainActivity.java
+++ app/src/main/java/com/esamdevelopers/gamej/crafterapp/MainActivity.java
@@ -1,13 +1,98 @@
 package com.esamdevelopers.gamej.crafterapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new MainFragment())
+                .commit();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null)
+            navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void onUpdateSubTitle(String subTitle){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null)
+            toolbar.setSubtitle(subTitle);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (drawerLayout.isDrawerOpen(navigationView))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                Toast.makeText(this, "Configuraciones", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+
+        switch (id){
+            case R.id.nav_home:
+                fragment = new MainFragment();
+                break;
+        }
+
+        if (fragment != null)
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/esamdevelopers/gamej/crafterapp/MainFragment.java
@@ -0,0 +1,35 @@
+package com.esamdevelopers.gamej.crafterapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.app_bar_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity() instanceof MainActivity){
+            MainActivity activity = (MainActivity) getActivity();
+            activity.onUpdateSubTitle(getString(R.string.title_nav_home));
+        }
+    }
+}
